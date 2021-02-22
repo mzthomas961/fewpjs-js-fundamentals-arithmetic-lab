a14ff472175f710ca54c2dd0745943c4ae73caec
Matthew Thomas <mzthomas96@gmail.com>
2021-02-21 22:58:21 -0500

    commit tree 4139fa80beecd8080507ea800e98a6f2c7d58651

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+const random = Math.round(Math.random()) + 1;
+const num3 = 9;
+const num4 = 5;
+const mod = num3 % num4;
+const max = Math.max(10,20,3,4,5);
